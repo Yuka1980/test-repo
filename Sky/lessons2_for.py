b728f7d0e0691c08617aee05d1e52f8e351e9549
Yuka1980 <banova1@bk.ru>
2024-06-30 14:52:46 -0400

    задние 2

---
+++ Sky/lessons2_for.py
@@ -0,0 +1,11 @@
+# user_login = "adam"
+# user_pass = "Qwerty12345" 
+
+# login = input("login: ")
+# password = input("pass: ")
+
+# if (login == user_login) and (password == user_pass):
+   # print("Secret is open")
+# else:
+  #  print("Locked")
+
