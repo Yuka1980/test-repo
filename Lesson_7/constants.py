7c53dce8caecf996dd5fa4c6f9d9aab5a2927b33
Yuka1980 <banova1@bk.ru>
2024-08-18 01:43:39 -0400

    Домашнее задание 7

---
+++ Lesson_7/constants.py
@@ -0,0 +1,6 @@
+# первое задание
+Test_form_URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+# второе задание
+Calculator_URL = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+# третье задание
+Shop_URL = "https://www.saucedemo.com/"
